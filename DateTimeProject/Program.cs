35d9896ce30791f59cdd0bfa35b3e27b41e75354
Ionut Tret <ionutztret@gmail.com>
2019-12-12 16:37:24 +0200

    Add project files.

---
+++ DateTimeProject/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeProject
+{
+    class Program
+    {
+        public enum GenderStates
+        {
+            Male,
+            Female
+        }
+
+        static void Main(string[] args)
+        {
+            int BirthYear, BirthMonth, BirthDay;
+
+            Console.WriteLine("Insert your birth year in the following format 'YYYY':");
+            string YearString = Console.ReadLine();
+            Console.WriteLine("Insert your birth month:");
+            string MonthString = Console.ReadLine();
+            Console.WriteLine("Insert your birth day:");
+            string DayString = Console.ReadLine();
+            Console.WriteLine("Insert your gender (M/F):");
+            string GenderString = Console.ReadLine();
+
+            BirthYear = int.Parse(YearString);
+            BirthMonth = int.Parse(MonthString);
+            BirthDay = int.Parse(DayString);
+
+            DateTime BirthDate = new DateTime(BirthYear, BirthMonth, BirthDay);
+            DateTime now = DateTime.Now;
+
+            TimeSpan DateDiff = now - BirthDate;
+            int AgeOfUser = DateDiff.Days / 365;
+
+            Console.WriteLine("Age of user: {0}", AgeOfUser);
+
+            int? GenderNumeric;
+            GenderNumeric = null;
+
+            if (GenderString=="M" || GenderString== "m")
+            {
+                GenderStates currentState = GenderStates.Male;
+                GenderNumeric = (int)currentState;
+            }
+            else if (GenderString == "F" || GenderString== "f")
+            {
+                GenderStates currentState = GenderStates.Female;
+                GenderNumeric = (int)currentState;
+            }
+
+            if (GenderNumeric.HasValue)
+            {
+                bool RetiredUser = true;
+                if (GenderNumeric == 0)
+                {
+                    if (AgeOfUser < 65)
+                    {
+                        Console.WriteLine("You will retire at 65 yo.");
+                        RetiredUser = false;
+                    }
+                }
+                else if (GenderNumeric == 1)
+                {
+                    if (AgeOfUser < 63)
+                    {
+                        Console.WriteLine("You will retire at 63 yo.");
+                        RetiredUser = false;
+                    }
+                }
+                if (RetiredUser)
+                {
+                    Console.WriteLine("Retired");
+                }
+            }
+           
+        }
+    }
+}
---
+++ SevenStrings/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevenStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string StringDemo1, StringDemo2;
+            StringDemo1 = "AAa";
+            StringDemo2 = "bbb";
+            string StringFull = string.Concat(StringDemo1," ",StringDemo2);
+
+            Console.WriteLine($"Stringul creat este: '{StringFull}'");
+
+            string LowerCaseStringFull = StringFull.ToLower();
+            Console.WriteLine($"Stringul cu litere mici: {LowerCaseStringFull}");
+
+            if (string.IsNullOrEmpty(StringFull))
+            {
+                Console.WriteLine("String null sau gol.");
+            }
+            else
+            {
+                int CountVowels = 0;
+                
+                string VowelsList = "a,e,i,o,u,A,E,I,O,U";
+                string[] SplitVowel = VowelsList.Split(',');
+                
+                for (int ii = 0; ii < LowerCaseStringFull.Length; ii++)
+                { 
+                    for (int jj = 0; jj < 10; jj++)
+                    {
+                        
+                        if (LowerCaseStringFull[ii] == VowelsList[jj])
+                        {
+                            CountVowels++;
+                        }
+                    }
+
+                }
+
+                Console.WriteLine($"Stringul are o lungime de {StringFull.Length} caractere din care {CountVowels} sunt vocale.");
+            }
+
+            string ReplaceStringFull = StringFull.Replace("a", "b");
+            Console.WriteLine($"Stringul dupa inlocuirea lui 'a' cu 'b': {ReplaceStringFull}");
+
+            if (string.Compare(StringFull, LowerCaseStringFull, true) == 0)
+            {
+                Console.WriteLine("Cele doua string-uri sunt identice");
+            }
+            else
+            {
+                Console.WriteLine("Cele doua string-uri sunt diferite");
+            }
+
+            if (StringFull.Contains("a"))
+            {
+                int indexOfa = StringFull.IndexOf('a');
+                Console.WriteLine($"Contine 'a' si apare pe pozitia: {indexOfa}");
+            }
+            else
+            {
+                Console.WriteLine("Nu contine 'a'.");
+            }
+        }
+    }
+}
